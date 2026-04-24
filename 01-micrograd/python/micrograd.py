a20b559eb61d21addb2a5d06bf39a0690946f196
Andrew <chebanov.andrew@gmail.com>
2026-04-24 19:08:43 +0200

    feat(01): add backward() topo sort and ops (pow, sub, neg, div, exp)

--- 01-micrograd/python/micrograd.py
+++ 01-micrograd/python/micrograd.py
@@ -56,5 +56,53 @@ class Value:
     def __rmul__(self, other):
         return self * other
 
+    def __pow__(self, other):
+        assert (isinstance(other, (int, float)))
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        return self * other ** -1
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
